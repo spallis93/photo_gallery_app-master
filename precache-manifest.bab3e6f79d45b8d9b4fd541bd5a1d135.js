859587355f32cca500e95dc0b056161e100b4888
Spyridon Pallis <spallis93@gmail.com>
2020-12-28 18:29:39 -0500

    Updates

---
+++ precache-manifest.bab3e6f79d45b8d9b4fd541bd5a1d135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8ea4b28b7c4ba2b48fea0ba2b170b8",
+    "url": "/spallis93/photo_gallery_app-master/index.html"
+  },
+  {
+    "revision": "2ebf365d9445bd7001cd",
+    "url": "/spallis93/photo_gallery_app-master/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "95de9335e266d54fbe86",
+    "url": "/spallis93/photo_gallery_app-master/static/css/main.962d1009.chunk.css"
+  },
+  {
+    "revision": "2ebf365d9445bd7001cd",
+    "url": "/spallis93/photo_gallery_app-master/static/js/2.8e2edbc7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/spallis93/photo_gallery_app-master/static/js/2.8e2edbc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95de9335e266d54fbe86",
+    "url": "/spallis93/photo_gallery_app-master/static/js/main.0540c472.chunk.js"
+  },
+  {
+    "revision": "3ff440c3365efb4fb5b0",
+    "url": "/spallis93/photo_gallery_app-master/static/js/runtime-main.a0fa1c07.js"
+  }
+]);
